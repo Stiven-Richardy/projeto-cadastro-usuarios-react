2aec10672f313559b523d030edb9679ddd3406f3
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-02-18 09:46:39 -0300

    Retirando a idade e colocando a senha com criptografia

--- cadastro-users-backend/server.js
+++ cadastro-users-backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import bcrypt from 'bcryptjs';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -7,6 +8,11 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+async function hashPassword(password) {
+    const salt = await bcrypt.genSalt(8);
+    return await bcrypt.hash(password, salt);
+}
 
 app.get('/usuarios', async (req, res) => {
     let users = [];
@@ -16,7 +22,7 @@ app.get('/usuarios', async (req, res) => {
             where: {
                 name: req.query.name,
                 email: req.query.email,
-                age: req.query.age
+                password: req.query.password
             }
         })
     } else {
@@ -27,17 +33,21 @@ app.get('/usuarios', async (req, res) => {
 });
 
 app.post('/usuarios', async (req, res) => {
+    const hashedPassword = await hashPassword(req.body.password);
+    
     await prisma.user.create({
         data: {
             name: req.body.name,
             email: req.body.email,
-            age: req.body.age
+            password: hashedPassword
         }
     })
     res.status(201).json({ message: 'Usuário criado com sucesso!' });
 });
 
 app.put('/usuarios/:id', async (req, res) => {
+    const hashedPassword = await hashPassword(req.body.password);
+
     await prisma.user.update({
         where: {
             id: req.params.id
@@ -45,7 +55,7 @@ app.put('/usuarios/:id', async (req, res) => {
         data: {
             name: req.body.name,
             email: req.body.email,
-            age: req.body.age
+            password: hashedPassword
         }
     })
     res.status(200).json({ message: 'Usuário atualizado com sucesso!' });
